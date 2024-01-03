19885a1c7738539c09b1f60ebd147365eaca5fcd
Pavel23791 <140964820+Pavel23791@users.noreply.github.com>
2024-01-03 18:11:38 +0200

    first initial

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+  
+  const loadDataFromServer = () => {
+    axios.get('http://localhost:5000/api/sendData')
+    .then(result => {
+      setMessage(result.data.message)
+    })
+    .catch(error => console.log(error.message))
+  }
+  useEffect(() => {
+    loadDataFromServer();
+  }, [])
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">Learn React</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
